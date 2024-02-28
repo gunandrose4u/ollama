d9a9f3b412fd39172ee90497fe6e5396fc7b5be9
Joe Zhu <jozh@microsoft.com>
2024-02-28 12:32:25 +0800

    first draft

--- llm/ggml.go
+++ llm/ggml.go
@@ -3,6 +3,7 @@ package llm
 import (
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 )
 
@@ -132,6 +133,9 @@ func (c *containerLORA) Decode(rso *readSeekOffset) (model, error) {
 	return nil, nil
 }
 
+type containerORT struct {
+}
+
 const (
 	// Magic constant for `ggml` files (unversioned).
 	FILE_MAGIC_GGML = 0x67676d6c
@@ -144,6 +148,7 @@ const (
 	// Magic constant for `gguf` files (versioned, gguf)
 	FILE_MAGIC_GGUF_LE = 0x46554747
 	FILE_MAGIC_GGUF_BE = 0x47475546
+	FILE_MAGIC_ORT     = 0x04034B50
 )
 
 var ErrUnsupportedFormat = errors.New("unsupported model format")
@@ -166,7 +171,10 @@ func DecodeGGML(r io.ReadSeeker) (*GGML, error) {
 		c = &containerGGUF{bo: binary.LittleEndian}
 	case FILE_MAGIC_GGUF_BE:
 		c = &containerGGUF{bo: binary.BigEndian}
+	case FILE_MAGIC_ORT:
+		c = &containerORT{}
 	default:
+		fmt.Printf("invalid file magic: %X\n", magic)
 		return nil, errors.New("invalid file magic")
 	}
 
